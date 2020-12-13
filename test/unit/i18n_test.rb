dbb278517c689123bd4464c8b9dfa52c6a55c433
imc <imc@okto7.com>
2020-12-13 12:22:20 +0200

    Edited test_locales_validness, to add bg.

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -19,13 +19,13 @@ class I18nTest < AdditionalTags::TestCase
   def test_locales_validness
     lang_files_count = Dir[Rails.root.join('plugins/additional_tags/config/locales/*.yml')].size
 
-    assert_equal 11, lang_files_count
+    assert_equal 12, lang_files_count
     valid_languages.each do |lang|
       assert set_language_if_valid(lang)
       case lang.to_s
       when 'en'
         assert_equal 'Merge selected tags', l(:label_merge_selected_tags)
-      when 'cs', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru'
+      when 'bg', 'cs', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru'
         assert_not l(:label_merge_selected_tags) == 'Merge selected tags', lang
       end
     end
