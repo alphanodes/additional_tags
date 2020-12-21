9cf19cff726933895414397f8564fca3d954aed3
Alexander Meindl <alex@alphanodes.com>
2020-12-21 11:09:36 +0100

    Fix for agile boards, if tags column is active

--- lib/additional_tags.rb
+++ lib/additional_tags.rb
@@ -40,6 +40,7 @@ module AdditionalTags
 
         if Redmine::Plugin.installed? 'redmine_agile'
           AgileQuery.include AdditionalTags::Patches::AgileQueryPatch
+          AgileBoardsController.include AdditionalTags::Patches::AgileBoardsControllerPatch
           AgileVersionsQuery.include(AdditionalTags::Patches::AgileVersionsQueryPatch) if AGILE_VERSION_TYPE == 'PRO version'
         end
       end
---
+++ lib/additional_tags/patches/agile_boards_controller_patch.rb
@@ -0,0 +1,14 @@
+module AdditionalTags
+  module Patches
+    module AgileBoardsControllerPatch
+      extend ActiveSupport::Concern
+
+      included do
+        helper :additional_tags
+        helper :additional_tags_issues
+
+        include AdditionalTagsHelper
+      end
+    end
+  end
+end
