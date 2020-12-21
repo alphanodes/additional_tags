df65343e3c7a00870b762b2786d51b7c8e9f8e70
Alexander Meindl <alex@alphanodes.com>
2020-12-21 11:42:28 +0100

    Agile versions is only available in PRO version

--- lib/additional_tags.rb
+++ lib/additional_tags.rb
@@ -41,8 +41,10 @@ module AdditionalTags
         if Redmine::Plugin.installed? 'redmine_agile'
           AgileQuery.include AdditionalTags::Patches::AgileQueryPatch
           AgileBoardsController.include AdditionalTags::Patches::AgileBoardsControllerPatch
-          AgileVersionsController.include AdditionalTags::Patches::AgileVersionsControllerPatch
-          AgileVersionsQuery.include(AdditionalTags::Patches::AgileVersionsQueryPatch) if AGILE_VERSION_TYPE == 'PRO version'
+          if AGILE_VERSION_TYPE == 'PRO version'
+            AgileVersionsController.include AdditionalTags::Patches::AgileVersionsControllerPatch
+            AgileVersionsQuery.include AdditionalTags::Patches::AgileVersionsQueryPatch
+          end
         end
       end
 
