3f737f630d911e5cdeed7253738ba37e407ecb94
Alexander Meindl <alex@alphanodes.com>
2021-11-28 17:27:44 +0100

    only remove unused tags once a minute

--- app/jobs/additional_tags_remove_unused_tag_job.rb
+++ app/jobs/additional_tags_remove_unused_tag_job.rb
@@ -2,6 +2,11 @@
 
 class AdditionalTagsRemoveUnusedTagJob < AdditionalTagsJob
   def perform
+    # only once a minute to reduce load
+    cache_key = self.class.to_s
+    return if Rails.cache.read cache_key
+
+    Rails.cache.write cache_key, true, expires_in: 60
     AdditionalTags::Tags.remove_unused_tags
   end
 end
