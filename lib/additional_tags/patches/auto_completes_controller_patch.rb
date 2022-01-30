f09eda6f26fd7f25b1aa3ced8de8c37baf746036
Alexander Meindl <alex@alphanodes.com>
2022-01-30 14:22:31 +0100

    AutoComplete cleanup

--- lib/additional_tags/patches/auto_completes_controller_patch.rb
+++ lib/additional_tags/patches/auto_completes_controller_patch.rb
@@ -7,14 +7,12 @@ module AdditionalTags
 
       included do
         include InstanceMethods
-        include AdditionalsQueriesHelper
       end
 
       module InstanceMethods
         def issue_tags
           suggestion_order = AdditionalTags.setting(:tags_suggestion_order) || 'name'
-          @name = build_search_query_term params
-          @tags = Issue.available_tags name_like: @name,
+          @tags = Issue.available_tags name_like: build_search_query_term(params),
                                        sort_by: suggestion_order,
                                        order: (suggestion_order == 'name' ? 'ASC' : 'DESC')
 
@@ -24,17 +22,17 @@ module AdditionalTags
         end
 
         def wiki_tags
-          @name = params[:q].to_s
-          @tags = WikiPage.available_tags project: nil, name_like: @name
+          @tags = WikiPage.available_tags project: nil,
+                                          name_like: build_search_query_term(params)
           render layout: false, partial: 'additional_tag_list', locals: { unsorted: true }
         end
 
         def all_tags
           return render_403 unless User.current.admin?
 
-          @name = params[:q].to_s
+          q = build_search_query_term params
           sql_for_where = "LOWER(#{ActiveRecord::Base.connection.quote_table_name ActsAsTaggableOn.tags_table}.name) LIKE ?"
-          @tags = ActsAsTaggableOn::Tag.where(sql_for_where, "%#{@name.downcase}%")
+          @tags = ActsAsTaggableOn::Tag.where(sql_for_where, "%#{q.downcase}%")
                                        .order(name: :asc)
 
           render layout: false, partial: 'additional_tag_list', locals: { unsorted: true }
