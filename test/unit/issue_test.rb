c3df5c64d761f95d9744e9a0dd100b7cba11b2a9
Makurin <dmitry.makurin@red-soft.ru>
2022-05-06 16:38:33 +0300

    Allow the parent project to access the tags of its subprojects

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -56,10 +56,10 @@ class IssueTest < AdditionalTags::TestCase
   end
 
   test 'available tags should allow list tags of specific project only' do
-    assert_equal 4, Issue.available_tags(project: @project_a).to_a.size
+    assert_equal 5, Issue.available_tags(project: @project_a).to_a.size
     assert_equal 1, Issue.available_tags(project: @project_b).to_a.size
 
-    assert_equal 3, Issue.available_tags(open_issues_only: true, project: @project_a).to_a.size
+    assert_equal 4, Issue.available_tags(open_issues_only: true, project: @project_a).to_a.size
     assert_equal 1, Issue.available_tags(open_issues_only: true, project: @project_b).to_a.size
   end
 
@@ -69,7 +69,7 @@ class IssueTest < AdditionalTags::TestCase
     assert_equal 2, Issue.available_tags(name_like: 's').to_a.size
     assert_equal 2, Issue.available_tags(name_like: 'e').to_a.size
 
-    assert_equal 2, Issue.available_tags(name_like: 'f', project: @project_a).to_a.size
+    assert_equal 3, Issue.available_tags(name_like: 'f', project: @project_a).to_a.size
     assert_equal 0, Issue.available_tags(name_like: 'b', project: @project_a).to_a.size
     assert_equal 1, Issue.available_tags(name_like: 'sec', open_issues_only: true, project: @project_a).to_a.size
     assert_equal 1, Issue.available_tags(name_like: 'fir', open_issues_only: true, project: @project_a).to_a.size
@@ -104,5 +104,19 @@ class IssueTest < AdditionalTags::TestCase
         issue.destroy!
       end
     end
+  end
+
+  def test_available_tags_with_project_option_should_respect_subprojects
+    # tag exists only in subproject
+    Issue.generate! project: @project_b, tag_list: 'im not exist in parent project'
+
+    new_tag = ActsAsTaggableOn::Tag.find_by name: 'im not exist in parent project'
+
+    assert_not_nil new_tag
+
+    # parent project should list all available tags including subprojects
+    assert_includes Issue.available_tags(project: @project_a).to_a, new_tag
+    # subproject
+    assert_includes Issue.available_tags(project: @project_b).to_a, new_tag
   end
 end
