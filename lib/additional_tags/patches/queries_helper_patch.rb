cc2cbe55b6fba4364dda4230ea33b04eceb35819
Alexander Meindl <alex@alphanodes.com>
2023-01-09 13:29:36 +0100

    #40 instance variable performance cache is not compatible with other plugins (e.g. agile), because there is no issues array available. Activate fallback for this case with expensive permission check

--- lib/additional_tags/patches/queries_helper_patch.rb
+++ lib/additional_tags/patches/queries_helper_patch.rb
@@ -15,7 +15,19 @@ module AdditionalTags
       module InstanceMethods
         def column_content_with_tags(column, item)
           if column.name == :issue_tags || item.is_a?(Issue) && column.name == :tags
-            additional_tag_links item.instance_variable_get(:@visible_tags), tag_controller: 'issues'
+            tags = if item.instance_variable_defined? :@visible_tags
+                     item.instance_variable_get :@visible_tags
+                   elsif Setting.display_subprojects_issues?
+                     # permission check required (expensive)
+                     return unless User.current.allowed_to? :view_issue_tags, item.project
+
+                     column.value item
+                   else
+                     # no permission check required
+                     column.value item
+                   end
+
+            additional_tag_links tags, tag_controller: 'issues'
           else
             column_content_without_tags column, item
           end
