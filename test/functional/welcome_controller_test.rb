22bc5524e305badda6fc16dff8a6c7cb6094c47b
Alexander Meindl <a.meindl@alphanodes.com>
2023-02-26 11:01:04 +0100

    do not use require_relative for test helper: this breaks file loading with tests

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../test_helper'
+require File.expand_path '../../test_helper', __FILE__
 
 class WelcomeControllerTest < AdditionalTags::ControllerTest
   fixtures :projects, :news, :users, :members,
--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../test_helper'
+require File.expand_path '../../test_helper', __FILE__
 
 class I18nTest < AdditionalTags::TestCase
   include Redmine::I18n
