884c1fc8d896b5e449f9e670a390dbdc22a874d4
Roman Petrenko <roman.l.petrenko@gmail.com>
2023-06-19 23:45:53 +0300

    fix: error if Query class does not exist

--- db/migrate/20230619165600_migrate_existing_queries.rb
+++ db/migrate/20230619165600_migrate_existing_queries.rb
@@ -2,15 +2,17 @@
 
 class MigrateExistingQueries < ActiveRecord::Migration[5.2]
   def up
-    Query.all.each do |query|
-      if query.filters.has_key?("issue_tags")
-        query.filters["tags"] = query.filters.delete("issue_tags")
+    Query.subclasses.each do |q|
+      query_class = Object.const_get q.class_name
+      query_class.all.each do |query|
+        if query.filters.has_key?("issue_tags")
+          query.filters["tags"] = query.filters.delete("issue_tags")
+        end
+        if  query.column_names.is_a?(Array) && query.column_names.include?(:tags_relations)
+          query.column_names.map! { |x| x == :tags_relations ? :tags : x }
+        end
+        query.save
       end
-
-      if  query.column_names.is_a?(Array) && query.column_names.include?(:tags_relations)
-        query.column_names.map! { |x| x == :tags_relations ? :tags : x }
-      end
-      query.save
     end
   end
 
