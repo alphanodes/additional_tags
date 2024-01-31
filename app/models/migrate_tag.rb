de112c84d8bd4e8525a49ee3ba70a45ef3ad66db
Alexander Meindl <a.meindl@alphanodes.com>
2024-01-31 09:32:02 +0100

    Use ApplicationRecord with Rails 7

--- app/models/migrate_tag.rb
+++ app/models/migrate_tag.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MigrateTag < ActiveRecord::Base
+class MigrateTag < Rails.version < '7.1' ? ActiveRecord::Base : ApplicationRecord
   self.table_name = 'tags'
   has_many :migrate_taggings, dependent: :destroy, foreign_key: :tag_id, inverse_of: :migrate_tag
 end
--- app/models/migrate_tagging.rb
+++ app/models/migrate_tagging.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MigrateTagging < ActiveRecord::Base
+class MigrateTagging < Rails.version < '7.1' ? ActiveRecord::Base : ApplicationRecord
   self.table_name = 'taggings'
   belongs_to :migrate_tag, foreign_key: :tag_id, inverse_of: :migrate_taggings
   belongs_to :taggable, polymorphic: true
