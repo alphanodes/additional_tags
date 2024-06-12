23f60c61077de134211e1d439d5552fe176c2fd1
Alexander Meindl <a.meindl@alphanodes.com>
2024-06-12 07:36:35 +0200

    use AdditionalsApplicationRecord for redmine 5.x and 6 support as temp solution

--- app/models/migrate_tag.rb
+++ app/models/migrate_tag.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MigrateTag < Rails.version < '7.1' ? ActiveRecord::Base : ApplicationRecord
+class MigrateTag < AdditionalsApplicationRecord
   self.table_name = 'tags'
   has_many :migrate_taggings, dependent: :destroy, foreign_key: :tag_id, inverse_of: :migrate_tag
 end
