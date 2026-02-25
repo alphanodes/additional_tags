e6eb8a8e8bc74bb25ca1d5a10a0931071b14e127
Alexander Meindl <a.meindl@alphanodes.com>
2026-02-25 16:56:28 +0100

    Upgrade ESLint 8 to 10 with flat config, Stylelint 14 to 17 and add curated rule sets

--- assets/javascripts/tags.js
+++ assets/javascripts/tags.js
@@ -1,19 +1,19 @@
 /* global mostUsedTags:writable */
-$(function() {
-  $('body').on('click', '.most-used-tags .most-used-tag', function(e) {
-    var $tagsSelect = $('select#issue_tag_list');
-    var tag = e.target.innerText;
-    if ($tagsSelect.find('option[value=\'' + tag + '\']').length === 0) {
-      var newOption = new Option(tag, tag, true, true);
+$(() => {
+  $('body').on('click', '.most-used-tags .most-used-tag', (e) => {
+    const $tagsSelect = $('select#issue_tag_list');
+    const tag = e.target.innerText;
+    if ($tagsSelect.find(`option[value='${  tag  }']`).length === 0) {
+      const newOption = new Option(tag, tag, true, true);
       $tagsSelect.append(newOption).trigger('change');
     }
 
-    mostUsedTags = $.grep(mostUsedTags, function(t) { return t != tag; });
-    var tagsHtml = mostUsedTags.map(function(tag) {
-      return '<span class="most-used-tag">' + tag + '</span>';
+    mostUsedTags = $.grep(mostUsedTags, (t) => { return t !== tag; });
+    const tagsHtml = mostUsedTags.map((tag) => {
+      return `<span class="most-used-tag">${  tag  }</span>`;
     }).join(', ');
 
-    var $mostUsedTagsContainer = $(e.target).parent('.most-used-tags');
+    const $mostUsedTagsContainer = $(e.target).parent('.most-used-tags');
     $mostUsedTagsContainer.empty();
     $mostUsedTagsContainer.append(tagsHtml);
   });
