9a90a1be7cc2aa95be3ce463bf50e371d968c9c4
Alexander Meindl <a.meindl@alphanodes.com>
2026-03-26 08:28:56 +0100

    Improve tags.js security and code quality with native JS methods

--- assets/javascripts/tags.js
+++ assets/javascripts/tags.js
@@ -2,19 +2,15 @@
 $(() => {
   $('body').on('click', '.most-used-tags .most-used-tag', (e) => {
     const $tagsSelect = $('select#issue_tag_list');
-    const tag = e.target.innerText;
-    if ($tagsSelect.find(`option[value='${  tag  }']`).length === 0) {
+    const tag = e.target.textContent;
+    if ($tagsSelect.find('option').filter((_, opt) => opt.value === tag).length === 0) {
       const newOption = new Option(tag, tag, true, true);
       $tagsSelect.append(newOption).trigger('change');
     }
 
-    mostUsedTags = $.grep(mostUsedTags, (t) => { return t !== tag; });
-    const tagsHtml = mostUsedTags.map((tag) => {
-      return `<span class="most-used-tag">${  sanitizeHTML(tag)  }</span>`;
-    }).join(', ');
+    mostUsedTags = mostUsedTags.filter((t) => t !== tag);
+    const tagsHtml = mostUsedTags.map((t) => `<span class="most-used-tag">${sanitizeHTML(t)}</span>`).join(', ');
 
-    const $mostUsedTagsContainer = $(e.target).parent('.most-used-tags');
-    $mostUsedTagsContainer.empty();
-    $mostUsedTagsContainer.append(tagsHtml);
+    $(e.target).parent('.most-used-tags').html(tagsHtml);
   });
 });
