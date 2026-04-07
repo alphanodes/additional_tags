775980a4b9abccf3f14675e7cb3441b3cbf1c68f
Alexander Meindl <a.meindl@alphanodes.com>
2026-04-07 20:52:26 +0200

    Fix tags name column collation from utf8mb4_bin to utf8mb4_unicode_ci for case-insensitive search

---
+++ db/migrate/20250407000000_fix_tags_collation.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class FixTagsCollation < ActiveRecord::Migration[7.2]
+  def up
+    return unless Redmine::Database.mysql?
+
+    table = ActsAsTaggableOn.tags_table
+    result = ActiveRecord::Base.connection.select_one(
+      'SELECT COLLATION_NAME FROM information_schema.COLUMNS ' \
+      "WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = '#{table}' AND COLUMN_NAME = 'name'"
+    )
+    return unless result && result['COLLATION_NAME']&.end_with?('_bin')
+
+    execute "ALTER TABLE #{table} MODIFY name varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+  end
+
+  def down
+    # intentionally left empty - reverting to _bin would break case-insensitive searches
+  end
+end
