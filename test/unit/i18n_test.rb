833491c1710a1871b5e047fd8732cf2b40a08d4a
Alexander Meindl <a.meindl@alphanodes.com>
2026-05-27 16:35:20 +0200

    Use i18n-tasks locale validation and complete label_tags_color_theme rename in eight languages

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -5,8 +5,6 @@ require File.expand_path '../../test_helper', __FILE__
 class I18nTest < AdditionalTags::TestCase
   Additionals.define_i18n_tests self,
                                 plugin: 'additional_tags',
-                                file_cnt: 13,
-                                locales: %w[bg cs de es fa fr it ja ko pl pt-BR ru],
                                 control_string: :label_merge_selected_tags,
                                 control_english: 'Merge selected tags'
 end
