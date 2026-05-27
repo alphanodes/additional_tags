31b2eb8b8723d4f5f9a6eee76fec7c7e6214b22b
Alexander Meindl <a.meindl@alphanodes.com>
2026-05-27 13:50:19 +0200

    Use additionals' define_i18n_tests helper to remove i18n_test boilerplate

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -3,18 +3,10 @@
 require File.expand_path '../../test_helper', __FILE__
 
 class I18nTest < AdditionalTags::TestCase
-  include Redmine::I18n
-
-  def test_valid_languages
-    assert_kind_of Array, valid_languages
-    assert_kind_of Symbol, valid_languages.first
-  end
-
-  def test_locales_validness
-    assert_locales_validness plugin: 'additional_tags',
-                             file_cnt: 13,
-                             locales: %w[bg cs de es fa fr it ja ko pl pt-BR ru],
-                             control_string: :label_merge_selected_tags,
-                             control_english: 'Merge selected tags'
-  end
+  Additionals.define_i18n_tests self,
+                                plugin: 'additional_tags',
+                                file_cnt: 13,
+                                locales: %w[bg cs de es fa fr it ja ko pl pt-BR ru],
+                                control_string: :label_merge_selected_tags,
+                                control_english: 'Merge selected tags'
 end
