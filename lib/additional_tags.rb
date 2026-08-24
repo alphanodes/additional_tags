3a9bc3a0ce7971cd84e58432ae6ccea77b159fe0
Alexander Meindl <a.meindl@alphanodes.com>
2026-08-24 17:24:43 +0200

    Raise the plugin requirement error with class and message instead of an explicit exception instance

--- lib/additional_tags.rb
+++ lib/additional_tags.rb
@@ -32,10 +32,9 @@ module AdditionalTags
       begin
         Redmine::Plugin.find 'additionals'
       rescue Redmine::PluginNotFound
-        # rubocop: disable Style/RaiseArgs
-        raise Redmine::PluginRequirementError.new "#{plugin_id} plugin requires the additionals plugin. " \
-                                                  'Please install additionals plugin (https://github.com/alphanodes/additionals)'
-        # rubocop: enable Style/RaiseArgs
+        raise Redmine::PluginRequirementError,
+              "#{plugin_id} plugin requires the additionals plugin. " \
+              'Please install additionals plugin (https://github.com/alphanodes/additionals)'
       end
 
       ActiveSupport.on_load(:active_record) { include AdditionalTags::Taggable }
